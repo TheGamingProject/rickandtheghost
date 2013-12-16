9d4fe053b7657ab13566a0037d8aa40a9591502f
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2013-12-16 17:11:46 -0600

    story.js added

---
+++ src/modules/StoryTimeline.js
@@ -0,0 +1,11 @@
+/**
+ * StoryTimeline.js
+ *
+ * Created by niko on 12/16/13.
+ */
+
+define([],function(){
+
+
+
+});
---
+++ src/modules/scenes/story.js
@@ -0,0 +1,52 @@
+/**
+ * story.js
+ *
+ * Created by niko on 12/16/13.
+ */
+
+define([],function(){
+  return {
+    name: "Rick and the Ghost",
+
+    scenes: [
+      "scene1",
+      "scene2"
+    ],
+
+
+    goals: [
+      {
+        name: "Suspense master",
+        desc: "you needa get super suspensed bro",
+        stats: [{
+          type: "suspense",
+          comparison: ">",  //player comparision amount
+          amount: 30 //player.suspense > 30
+        }]
+      },
+      {
+        name: "Lazy ",
+        desc: "be the laziest ghost",
+        stats: [{
+          type: "suspense",
+          comparison: "<",
+          amount: 10
+        },{
+          type: "scared",
+          comparison: "<",
+          amount: 10
+        },{
+          type: "goodday",
+          comparison: "<",
+          amount: 10
+        },{
+          type: "goodday",
+          comparison: ">",
+          amount: -10
+        }]
+      }
+    ]
+
+  };
+
+});
